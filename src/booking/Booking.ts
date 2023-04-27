5ffda0d85f0574d9ffef5edecb47d8234c4e7aa8
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-04-27 17:57:56 +0700

    Update booking

--- src/booking/Booking.ts
+++ src/booking/Booking.ts
@@ -1,3 +1,9 @@
 export class Booking{
-    
+    constructor(public name: string, public passport: string, public flightcode: string, public national: string, public amounth: number){
+        this.name = name;
+        this.passport = passport;
+        this.flightcode = flightcode;
+        this.national = national;
+        this.amounth = amounth;
+    }
 }
