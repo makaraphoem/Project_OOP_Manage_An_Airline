73f63c05809597561d18123766d482712f9b7f4a
sophorntoeum <sophorn.toeum@student.passerellesnumeriques.org>
2023-04-28 10:03:15 +0700

    Import class and create object

--- src/Main.ts
+++ src/Main.ts
@@ -4,7 +4,7 @@ import { Booking } from "./booking/Booking";
 import { Flight } from "./flight/Flight";
 import { DateTime } from "./flight/DateTime";
 import { Layout } from "./flight/Layout";
-import { Meal } from "./flight/Meal";
+import { Meal, MealType } from "./flight/Meal";
 import { Route } from "./flight/Route";
 import { Seat } from "./flight/Seat";
 import { Trip } from "./flight/Trip";
@@ -14,9 +14,13 @@ import { Airport } from "./Airport";
 import { Employee } from "./person/emloyee/Employee";
 
 //Grate Employees
-let emplyee = new Employee('phorn', 'So', 20,Gender.FEMALE)
+let emplyee = new Employee('phorn', 'So', 20,Gender.FEMALE);
 console.log(emplyee);
+//Grate Date
+let dateTime = new DateTime(28,4,2023,8);
+console.log(dateTime);
 
-//Grate Airline
-// let airline = new Airline('soo')
-// airline.employees()
+//Create Meal
+let meal = new Meal('Piza', MealType.kosher);
+console.log(meal);
+
